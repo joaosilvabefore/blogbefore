5132e111e4e2a22b111b9ad4793262a6769c58dd
Ygor Azambuja <ygorazambuja@gmail.com>
2021-11-04 19:21:40 -0400

    commit 02

---
+++ src/pages/Posts.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>All Posts</h2>
+    <div v-for="edge in $page.posts.edges" :key="edge.node.id">
+      <h2>{{ edge.node.id }}</h2>
+    </div>
+  </div>
+</template>
+
+<page-query>
+query {
+  posts: allPost {
+    edges {
+      node {
+        id
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {};
+</script>
+
+<style></style>
